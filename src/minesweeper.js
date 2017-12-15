74dd98fecab15d9b5b662e97d32a3128e97165f8
Olga Ialanskaia <ialanskaia@Olgas-MacBook-Pro.local>
2017-12-15 14:21:25 +0100

    added flip a tile function

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -28,12 +28,50 @@ const generateBombBoard = (numberOfRows, numberOfColumns, numbersOfBombs) => {
     const randomRowIndex = Math.floor(Math.random() * numberOfRows);
     const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
 
-    board[randomRowIndex][randomColumnIndex] = 'B';
-
-    numberOfBombsPlaced++;
+    if(board[randomRowIndex][randomColumnIndex] !== 'B'){
+      board[randomRowIndex][randomColumnIndex] = 'B';
+      numberOfBombsPlaced++;
+    }
   }
   return board;
 };
+
+const getNumberOfNeighborBombs = (bombBoard, rowIndex, columnIndex) => {
+  const neighborOffsets = [
+    [-1, -1],
+    [-1, 0],
+    [-1, 1],
+    [0, -1],
+    [0, 1],
+    [1, -1],
+    [1, 0],
+    [1, 1]
+  ];
+  const numberOfRows = bombBoard.length;
+  const numberOfColumns = bombBoard[0].length;
+  let numberOfBombs = 0;
+
+  neighborOffsets.forEach(offset => {
+    const neighborRowIndex = rowIndex + offset[0];
+    const neighborColumnIndex = columnIndex + offset[0];
+    if(neighborRowIndex >=0 && neighborRowIndex < numberOfRows && neighborColumnIndex >=0 && neighborColumnIndex < numberOfColumns) {
+      if(bombBoard[neighborRowIndex][neighborColumnIndex] == 'B') {
+        numberOfBombs++;
+      }
+    }
+  });
+  return numberOfBombs;
+}
+
+const flipTile = (playerBoard, bombBoard, rowIndex, columnIndex) => {
+  if(playerBoard[rowIndex][columnIndex] !== ' ') {
+    return 'This tile has already been flipped!';
+  } else if (bombBoard[rowIndex][columnIndex] == 'B') {
+    playerBoard[rowIndex][columnIndex] = 'B';
+  } else {
+    playerBoard[rowIndex][columnIndex] = getNumberOfNeighborBombs(bombBoard, rowIndex, columnIndex);
+  }
+}
 
 const printBoard = board => {
   console.log(board.map(row => row.join(' | ')).join('\n'));
@@ -46,3 +84,7 @@ console.log('Player Board: ')
 printBoard(playerBoard);
 console.log('Bomb Board: ')
 printBoard(bombBoard);
+
+flipTile(playerBoard, bombBoard, 1, 0);
+console.log('Updated Player Board:');
+printBoard(playerBoard);
